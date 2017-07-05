383714d98324ce1cc94baa4b0dbbdc634754061b
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2017-07-05 16:16:50 -0400

    remove button

--- script.js
+++ script.js
@@ -2,22 +2,12 @@ var myform = document.querySelector('#todoForm');
 var todoDiv = document.querySelector('#task')
 var todos = document.querySelector('#todos')
 
-//button
-
-
-var button = '';
-//btnNew.onclick = function() {
-//  var inItemText = document.getElementById("inItemText");
 
 
 myform.addEventListener('submit', function (event) {
     event.preventDefault();
     var newTask = document.querySelector('#task').value
     var newDiv = document.createElement('div');
-    button = document.createElement('button')
-    button.textContent = 'Edit'
     newDiv.appendChild(document.createTextNode(newTask));
-    todos.prepend(button);
     todos.prepend(newDiv);
 })
-

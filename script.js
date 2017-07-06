4abd3716bf5e9c37c000021567d7efa1d1aa15c0
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2017-07-06 08:41:33 -0400

    checkbox replace, not fully fn

--- script.js
+++ script.js
@@ -6,8 +6,42 @@ var todos = document.querySelector('#todos')
 
 myform.addEventListener('submit', function (event) {
     event.preventDefault();
-    var newTask = document.querySelector('#task').value
+    //    set user input to 'NewTask'
+    var newTask = todoDiv.value
+    //    create containing div
     var newDiv = document.createElement('div');
-    newDiv.appendChild(document.createTextNode(newTask));
+    //    create input (for checkbox)
+    var newCheck = document.createElement('input')
+    //    create checkbox label
+    var newLabel = document.createElement('label')
+
+
+    //    assign input as type: checkbox
+    newCheck.type = 'checkbox'
+    //    set checkbox id
+    newCheck.id = 'checkboxTD'
+
+    //    set label text as user input
+    newLabel.innerHTML = newTask
+    //    set label class for buttonClicker
+    newLabel.id = 'labelCB'
+    //    connect label to checkbox
+    newLabel.setAttribute('for', 'checkboxTD')
+
+
+    //    append checkbox and label to div
+    newDiv.appendChild(newCheck)    
+    newDiv.appendChild(newLabel)
+    //    prepend div to existing div 'todos'
     todos.prepend(newDiv);
 })
+
+
+
+
+
+
+//     TODO: 
+//      edit field
+//      removed text after entry
+//      strikethrough

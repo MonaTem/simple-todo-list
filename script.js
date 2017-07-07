95c494601d14cf97d2619d75fcffbb037ffc8915
Delmer <delmer814+1@gmail.com>
2017-07-07 15:51:12 -0400

    Add comments and refactoring

--- script.js
+++ script.js
@@ -1,59 +1,98 @@
-const app = document.querySelector('#app');
-const input = document.querySelector('#input-form');
-const button = document.querySelector('#submit-button');
-const list = document.querySelector('#todo-list');
-const clearBtn = document.querySelector('#clear-button');
+(function App({els}) {
+  els.button.addEventListener('click', function() {
+    console.log('hello');
+    addTodo(els)
+  });
 
-button.addEventListener('click', function() {
-  console.log('hello');
-  createToDo();
-});
+  els.clearBtn.addEventListener('click', function() {
+    clearTodos(els);
+  });
 
-clearBtn.addEventListener('click', function() {
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
+  els.input.addEventListener('keydown', function(event) {
+  	if (event.keyCode == 13) {
+      addTodo(els);
+    }
+  });
+
+  function clearTodos({list}) {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
   }
-});
 
-input.addEventListener('keydown', function(event) {
-	if (event.keyCode == 13) {
-		createToDo();
-	}
-});
+  function addTodo({list, input}) {
+    list.appendChild(Todo.create(input).el)
+    input.value = "";
+  }
+
+  const Todo = (function () {
+    const CON_NAME       = 'todo-container';
+    const COMPLETE_CLASS = 'completed';
+
+    function Todo({el, value, checkbox, label}) {
+      // Create a todo container
+      this.el           = el;
+      this.checkbox     = checkbox;
+      this.label        = label
+
+      this.add(this.checkbox).add(this.label);
+    }
+
+    Todo.create = function ({value}) {
+      let checkbox = new CheckBox();
+
+      const todo = new Todo({
+        el:       this.createEl(),
+        checkbox: checkbox,
+        label:    new Label({ content: value })
+      });
+
+      checkbox.click(todo, todo.complete)
+
+      return todo;
+    }
 
 
-function createToDo() {
-  // Create a todo container
-  const container = document.createElement('div');
+    Todo.createEl = function () {
+      const el     = document.createElement('div');
+      el.className = CON_NAME;
 
-  const checkbox = document.createElement('input');
-  checkbox.setAttribute('type', 'checkbox');
-  checkbox.className = 'checkbox';
+      return el;
+    }
 
-  // add event listener
-  checkbox.addEventListener('click', toggleCompleted);
+    Todo.prototype.add = function (child) {
+      this.el.appendChild(child.el);
 
-  const label = document.createElement('label');
-  container.className = 'todo-container';
-  label.innerText = input.value;
+      return this;
+    }
 
-  container.appendChild(checkbox);
-  container.appendChild(label);
-  list.appendChild(container);
+    Todo.prototype.complete = function (event) {
+      this.el.classList.toggle(COMPLETE_CLASS);
+    }
 
+    function Label({content}) {
+      this.el           = document.createElement('label');
+      this.el.innerText = content;
+    }
 
-  // clear the input form
-  input.value = '';
-}
+    function CheckBox() {
+      this.el = document.createElement('input');
+      this.el.setAttribute('type', 'checkbox');
+      this.el.className = 'checkbox';
+    }
 
-// toggles the completed class
-function toggleCompleted(event) {
-  const container = event.target.parentNode;
-  container.classList.toggle('completed');
-}
+    CheckBox.prototype.click = function (context, onClick) {
+      this.el.addEventListener("click", onClick.bind(context))
+    }
 
-// Background color transitions
-//function cycleColors() {
-//  let currentColor = document.body.style.backgroundColor;
-//  console.dir(currentColor);
-//}
+    return Todo;
+  })()
+})({
+  els: {
+    app:      document.querySelector('#app'),
+    input:    document.querySelector('#input-form'),
+    button:   document.querySelector('#submit-button'),
+    list:     document.querySelector('#todo-list'),
+    clearBtn: document.querySelector('#clear-button')
+  }
+})
